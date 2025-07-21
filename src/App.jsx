c66077d1b045c56c0b76787dcdf13adf73cbe538
Samuel Silva dos Santos <akitadecumace@gmail.com>
2025-07-20 22:58:53 -0300

    atualização do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
+import Perfil from "./components/Perfil";
+import Formulario from "./components/Formulario";
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [formularioEstaVisivel, setFromularioEstaVisivel] = useState(true);
+
+return(
+  
+  <>
+  <Perfil nome="samuel Silva dos Santos" endereco="https://github.com/SamuellSSantos.png"/>
+
+
+  {formularioEstaVisivel && (
+    <Formulario/>
+  )}
+  <button onClick={()=> setFromularioEstaVisivel(!formularioEstaVisivel)} type="button">toggle form</button>
+  </>
+  
+)
 }
+
 
 export default App
---
+++ src/components/Formulario/index.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react"
+
+
+
+const Formulario =() => {
+    const [materiaA, setMateriaA] = useState(0);
+    const [materiaB, setMateriaB] = useState(0);
+    const [materiaC, setMateriaC] = useState(0);
+    const [nome, setNome] = useState('');
+
+    useEffect (() => {
+        console.log("O componenete iniciou");
+
+        return () => {
+            console.log("O componente finalizou")
+        }
+    }, []);
+    
+    useEffect (() => {
+        console.log("O estado nome mudou");
+    }, [nome]);
+
+    useEffect (()=> {console.log("A materia A foi alterada para " + materiaA)}, [materiaA])
+
+    const alteraNome =(evento) =>{
+        //console.log(evento.target.value)
+        setNome(evento.target.value)
+    }
+
+
+const renderizaResultado = () => {
+    const  soma = materiaA + materiaB + materiaC;
+    const media = soma/3;
+
+    if(media >= 7 ){ 
+        return (
+            <p>Olá {nome},Você foi aprovado</p>
+        )
+    } else{
+        return(
+            <p>
+                Olá {nome},Você não passou de ano
+            </p>
+        )
+    }
+    
+
+}
+    return (
+        <form>
+            <input type="text" placeholder="Seu nome" onChange={alteraNome} />
+            <input type="number" placeholder= " Nota matéria A" onChange={({target}) => setMateriaA(parseInt(target.value))} />
+            <input type="number" placeholder= " Nota matéria B" onChange={evento => setMateriaB(parseInt(evento.target.value))} />
+            <input type="number" placeholder= " Nota matéria C" onChange ={evento => setMateriaC(parseInt(evento.target.value))} />
+            {renderizaResultado()}
+        </form>
+    )
+}
+
+export default Formulario
